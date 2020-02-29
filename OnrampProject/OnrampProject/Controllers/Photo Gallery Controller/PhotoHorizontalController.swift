483a8303e22dfcc3b60d1ccc3f6019a1b5d5f27d
Ramon Geronimo <rgero215@gmail.com>
2020-02-29 12:21:13 -0800

    render dummy cells on photo cell

--- OnrampProject/OnrampProject/Controllers/Photo Gallery Controller/PhotoHorizontalController.swift
+++ OnrampProject/OnrampProject/Controllers/Photo Gallery Controller/PhotoHorizontalController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class PhotoHorizontalController: BaseCollectionViewController {
     
+    // MARK:- PROPERTIES
+    let cellId = "cellId"
+    
+    
+    // MARK:- LIFE CYCLE
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    
+    }
+    
+    // MARK:- COLLECTION VIEW PROTOCOLS
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         
+        cell.backgroundColor = .red
+        
+        return cell
     }
 }
