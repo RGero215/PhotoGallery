d0147ba018e39cac3eed6933d3228c93670a7ccb
Ramon Geronimo <rgero215@gmail.com>
2020-02-29 02:12:39 -0800

    modified SceneDelegate to load view programmatically

--- OnrampProject/OnrampProject/Controllers/Root View Controller/RootViewController.swift
+++ OnrampProject/OnrampProject/Controllers/Root View Controller/RootViewController.swift
@@ -14,6 +14,7 @@ final class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 
 
