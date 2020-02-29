98529183e372f5c9038e4b90ec7c27ae8817afce
Ramon Geronimo <rgero215@gmail.com>
2020-02-29 02:39:14 -0800

    add tabBar assets and createNavController func

--- OnrampProject/OnrampProject/Controllers/Root View Controller/RootViewController.swift
+++ OnrampProject/OnrampProject/Controllers/Root View Controller/RootViewController.swift
@@ -15,6 +15,26 @@ final class RootViewController: UITabBarController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Photo Gallery", imageName: "iphoto-gallery"),
+            createNavController(viewController: UIViewController(), title: "AR Gallery", imageName: "ARKit"),
+            createNavController(viewController: UIViewController(), title: "User", imageName: "user"),
+        ]
+    }
+    
+    //MARK: - CREATE NAV CONTROLLER
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationController?.title = title
+        viewController.view.backgroundColor = .white
+        navController.tabBarController?.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        
+        return navController
     }
 
 
