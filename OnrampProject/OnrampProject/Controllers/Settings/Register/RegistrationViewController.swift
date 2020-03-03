fea3a0fbc64fd0141d706e9649c99f55892240b6
Ramon Geronimo <rgero215@gmail.com>
2020-03-03 14:58:33 -0800

    handle keyboard notifications in registration view

--- OnrampProject/OnrampProject/Controllers/Settings/Register/RegistrationViewController.swift
+++ OnrampProject/OnrampProject/Controllers/Settings/Register/RegistrationViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
-    
+    //MARK:- PROPERTIES
     lazy var width = view.frame.width
+    lazy var stackView = VerticalStackView(arrangedSubviews: [selectPhotoButton, fullNameTextField, emailTextField, passwordTextField, register])
     
     //MARK:- UI COMPONENTS
     let backgroundImage: UIImageView = {
@@ -77,22 +78,30 @@ class RegistrationViewController: UIViewController {
     
     
     //MARK:- LIFE CYCLE
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+    
         navigationController?.isNavigationBarHidden = true
-        view.addSubview(backgroundImage)
-        backgroundImage.fillSuperview()
-
-        let stackView = VerticalStackView(arrangedSubviews: [selectPhotoButton, fullNameTextField, emailTextField, passwordTextField, register])
-        
-        view.addSubview(stackView)
-        stackView.spacing = 8
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
-        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        setupLayout()
+        setupNotificationObservers()
+        setupTapGesture()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
     }
     
     //MARK:- FILEPRIVATE METHODS
+    
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        self.view.endEditing(true)
+    }
     
     @objc fileprivate func handleRegister() {
         
@@ -100,5 +109,34 @@ class RegistrationViewController: UIViewController {
     
     @objc fileprivate func handleGoToLogin() {
         
+    }
+    
+    fileprivate func setupLayout() {
+        view.addSubview(backgroundImage)
+        backgroundImage.fillSuperview()
+        view.addSubview(stackView)
+        stackView.spacing = 8
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    fileprivate func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return }
+        let keyboardFrame = value.cgRectValue
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        let difference = keyboardFrame.height - bottomSpace
+        self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
+    }
+    
+    @objc fileprivate func handleKeyboardHide(notification: Notification){
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut
+            , animations: {
+                self.view.transform = .identity
+        })
     }
 }
