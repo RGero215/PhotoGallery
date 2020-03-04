c7bb4eefdc45da67cb9a5e893dc4e9785461ec56
Ramon Geronimo <rgero215@gmail.com>
2020-03-03 20:21:52 -0800

    refactor registrationViewController and create registrationView

--- OnrampProject/OnrampProject/Controllers/Settings/Register/RegistrationViewController.swift
+++ OnrampProject/OnrampProject/Controllers/Settings/Register/RegistrationViewController.swift
@@ -8,102 +8,26 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
-    //MARK:- PROPERTIES
-    lazy var width = view.frame.width
-    var activeTextField: UITextField?
+    //MARK:- REGISTRATION VIEW
+    var registrationView = RegistrationView()
     
-    
-    //MARK:- UI COMPONENTS
-    let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.isDirectionalLockEnabled = true
-        scrollView.contentInsetAdjustmentBehavior = .never
-        return scrollView
-    }()
-    
-    lazy var verticalStackView: VerticalStackView = {
-        let stackView = VerticalStackView(arrangedSubviews: [fullNameTextField, emailTextField, passwordTextField, register])
-        stackView.distribution = .fillEqually
-        stackView.spacing = 8
-        return stackView
-    }()
-    
-    lazy var overallStackView = UIStackView(arrangedSubviews: [selectPhotoButton, verticalStackView])
-    
-    let backgroundImage: UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: "Untitled_Artwork 5")
-        iv.contentMode = .scaleAspectFill
-        return iv
-    }()
-    
-    lazy var selectPhotoButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Select Photo", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
-        button.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        button.setTitleColor(.black, for: .normal)
-        button.heightAnchor.constraint(equalToConstant:  width / 2).isActive = true
-        button.layer.cornerRadius = 16
-        return button
-    }()
-    
-    let fullNameTextField: CustomTexField = {
-        let textField = CustomTexField(padding: 24)
-        textField.placeholder = RegistrationPlaceHolder.name
-        textField.backgroundColor = .white
-        return textField
-    }()
-    
-    let emailTextField: CustomTexField = {
-        let textField = CustomTexField(padding: 24)
-        textField.placeholder = RegistrationPlaceHolder.email
-        textField.keyboardType = .emailAddress
-        textField.backgroundColor = .white
-        return textField
-    }()
-    
-    let passwordTextField: CustomTexField = {
-        let textField = CustomTexField(padding: 24)
-        textField.placeholder = RegistrationPlaceHolder.password
-        textField.isSecureTextEntry = true
-        textField.backgroundColor = .white
-        return textField
-    }()
-    
-    let register: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Register", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
-        button.backgroundColor = .black
-        button.isEnabled = false
-        button.setTitleColor(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), for: .disabled)
-        button.layer.cornerRadius = 16
-        button.setTitleColor(.black, for: .normal)
-        button.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
-        return button
-    }()
-    
-    let goToLoginButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Go to login", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .heavy)
-        button.addTarget(self, action: #selector(handleGoToLogin), for: .touchUpInside)
-        return button
-    }()
-    
+    //MARK:- LOAD VIEWS
+    override func loadView() {
+        view = registrationView
+        
+    }
     
     //MARK:- LIFE CYCLE
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+
         navigationController?.isNavigationBarHidden = true
-        setupLayout()
         setupNotificationObservers()
         setupTapGesture()
-        scrollView.delegate = self
+        setupButtons()
+        registrationView.scrollView.delegate = self
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -115,23 +39,23 @@ class RegistrationViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-
+        registrationView.scrollView.contentSize = view.frame.size
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         if UIDevice.current.orientation.isLandscape {
             handleTapDismiss()
-            overallStackView.axis = .horizontal
+            registrationView.overallStackView.axis = .horizontal
         } else {
             handleTapDismiss()
-            overallStackView.axis = .vertical
+            registrationView.overallStackView.axis = .vertical
         }
     }
     
     
     //MARK:- TEXTFIELD DID BEGIN EDITING
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        activeTextField = textField
+        registrationView.activeTextField = textField
     }
     
     //MARK:- FILEPRIVATE METHODS
@@ -153,22 +77,6 @@ class RegistrationViewController: UIViewController {
         
     }
     
-    
-    //MARK:- SETUP LAYOUTS
-    fileprivate func setupLayout() {
-        view.addSubview(backgroundImage)
-        backgroundImage.fillSuperview()
-        view.addSubview(scrollView)
-        scrollView.fillSuperview()
-        scrollView.contentSize = view.frame.size
-        scrollView.addSubview(overallStackView)
-        overallStackView.spacing = 8
-        overallStackView.axis = .vertical
-        selectPhotoButton.widthAnchor.constraint(equalToConstant: 275).isActive = true
-        overallStackView.anchor(top: nil, leading: backgroundImage.leadingAnchor, bottom: nil, trailing: backgroundImage.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
-        overallStackView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor).isActive = true
-    }
-    
     //MARK:- OBSERVERS
     fileprivate func setupNotificationObservers() {
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -180,17 +88,17 @@ class RegistrationViewController: UIViewController {
     @objc fileprivate func handleKeyboardShow(notification: Notification) {
         guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return }
         let keyboardFrame = value.cgRectValue
-        let bottomSpace = view.frame.height - scrollView.frame.origin.y - scrollView.frame.height
+        let bottomSpace = view.frame.height - registrationView.scrollView.frame.origin.y - registrationView.scrollView.frame.height
         
         let difference = keyboardFrame.height - bottomSpace
         self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
 
-        scrollView.contentInset.top = keyboardFrame.height
+        registrationView.scrollView.contentInset.top = keyboardFrame.height
         
         // automatically scroll to visible active text field
-        guard let activeTextField = activeTextField else { return }
-        let visibleRect = activeTextField.convert(activeTextField.bounds, to: scrollView)
-        scrollView.scrollRectToVisible(visibleRect, animated: true)
+        guard let activeTextField = registrationView.activeTextField else { return }
+        let visibleRect = activeTextField.convert(activeTextField.bounds, to: registrationView.scrollView)
+        registrationView.scrollView.scrollRectToVisible(visibleRect, animated: true)
     }
     
     //MARK:- KEYBOARD HIDE
@@ -198,6 +106,9 @@ class RegistrationViewController: UIViewController {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut
             , animations: {
                 self.view.transform = .identity
+                self.registrationView.scrollView.contentInset.top = 0
+                self.registrationView.scrollView.verticalScrollIndicatorInsets.top = 0
+                
         })
     }
 }
@@ -207,5 +118,14 @@ extension RegistrationViewController: UIScrollViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         scrollView.contentOffset.x = 0
+    }
+}
+
+//MARK:- SETUP BUTTON ACTIONS
+extension RegistrationViewController {
+    fileprivate func setupButtons() {
+
+        registrationView.register.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
+        registrationView.goToLoginButton.addTarget(self, action: #selector(handleGoToLogin), for: .touchUpInside)
     }
 }
---
+++ OnrampProject/OnrampProject/Views/Settings/Register/RegistrationView.swift
@@ -0,0 +1,119 @@
+//
+//  RegistrationView.swift
+//  OnrampProject
+//
+//  Created by Ramon Geronimo on 3/3/20.
+//
+
+import UIKit
+
+class RegistrationView: UIView  {
+    
+    //MARK:- PROPERTIES
+    var activeTextField: UITextField?
+    
+    //MARK:- UI COMPONENTS
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isDirectionalLockEnabled = true
+        scrollView.contentInsetAdjustmentBehavior = .never
+        return scrollView
+    }()
+    
+    lazy var verticalStackView: VerticalStackView = {
+        let stackView = VerticalStackView(arrangedSubviews: [fullNameTextField, emailTextField, passwordTextField, register])
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    lazy var overallStackView = UIStackView(arrangedSubviews: [selectPhotoButton, verticalStackView])
+    
+    let backgroundImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "Untitled_Artwork 5")
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    lazy var selectPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Select Photo", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        button.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        button.setTitleColor(.black, for: .normal)
+        button.heightAnchor.constraint(equalToConstant: 275).isActive = true
+        button.layer.cornerRadius = 16
+        button.imageView?.contentMode = .scaleAspectFill
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    let fullNameTextField: CustomTexField = {
+        let textField = CustomTexField(padding: 24)
+        textField.placeholder = RegistrationPlaceHolder.name
+        textField.backgroundColor = .white
+        return textField
+    }()
+    
+    let emailTextField: CustomTexField = {
+        let textField = CustomTexField(padding: 24)
+        textField.placeholder = RegistrationPlaceHolder.email
+        textField.keyboardType = .emailAddress
+        textField.backgroundColor = .white
+        return textField
+    }()
+    
+    let passwordTextField: CustomTexField = {
+        let textField = CustomTexField(padding: 24)
+        textField.placeholder = RegistrationPlaceHolder.password
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = .white
+        return textField
+    }()
+    
+    let register: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Register", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        button.backgroundColor = .black
+        button.isEnabled = false
+        button.setTitleColor(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), for: .disabled)
+        button.layer.cornerRadius = 16
+        button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
+    
+    let goToLoginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Go to login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .heavy)
+        return button
+    }()
+    
+    // MARK:- INITIALIZER
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    //MARK:- SETUP LAYOUTS
+    fileprivate func setupLayout() {
+        addSubview(backgroundImage)
+        backgroundImage.fillSuperview()
+        addSubview(scrollView)
+        scrollView.fillSuperview()
+        scrollView.addSubview(overallStackView)
+        overallStackView.spacing = 8
+        overallStackView.axis = .vertical
+        selectPhotoButton.widthAnchor.constraint(equalToConstant: 275).isActive = true
+        overallStackView.anchor(top: nil, leading: backgroundImage.leadingAnchor, bottom: nil, trailing: backgroundImage.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        overallStackView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
