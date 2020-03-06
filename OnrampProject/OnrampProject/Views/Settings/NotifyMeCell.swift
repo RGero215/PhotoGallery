13a35bac0aa7374f08e02286f4a78940b826b626
Ramon Geronimo <rgero215@gmail.com>
2020-03-05 16:47:31 -0800

    added segmentedControl to the settings

--- OnrampProject/OnrampProject/Views/Settings/NotifyMeCell.swift
+++ OnrampProject/OnrampProject/Views/Settings/NotifyMeCell.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class NotifyMeCell: UITableViewCell {
+    
+    let bookSegment: UISegmentedControl = {
+        let item = ["No", "Yes"]
+        let segment = UISegmentedControl(items: item)
+        segment.selectedSegmentIndex = 0
+        segment.layer.cornerRadius = 5.0
+        segment.backgroundColor = .black
+        segment.tintColor = .white
+        return segment
+    }()
+    
+    let gameSegment: UISegmentedControl = {
+        let item = ["No", "Yes"]
+        let segment = UISegmentedControl(items: item)
+        segment.selectedSegmentIndex = 0
+        segment.layer.cornerRadius = 5.0
+        segment.backgroundColor = .black
+        segment.tintColor = .white
+        return segment
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        backgroundColor = .blue
+        let stackView = VerticalStackView(arrangedSubviews: [bookSegment, gameSegment])
+        addSubview(stackView)
+        stackView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
